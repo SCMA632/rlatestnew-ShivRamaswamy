a2d5b8d6964ff5650b4c3b93cdaa38c0998beb3d
ShivRamaswamy <86184386+ShivRamaswamy@users.noreply.github.com>
2021-10-23 19:28:56 +0530

    Update test1.R

--- test1.R
+++ test1.R
@@ -4,4 +4,42 @@ names(df)
 summary(df)
 
 unique(df$state)
+meg = df[df$state_1 == 'MEG',]
+
+# Data type of the subset data
+class(meg)
+
+any(is.na(meg))
+
+# Shape of the Subset
+dim(meg)
+
+# To View the filtered data
+#View(meg)
+
+# Different columns
+names(meg)
+
+# Top 3 rows of the subset
+head(meg,3)
+
+# Bottom 3 rows of the subset
+tail(meg,3)
+
+# Unique Districts of the subset
+unique(meg$District)
+
+str(meg)
+
+#View(meg)
+
+# takes lot time to generate a report
+#create_report(meg)
+
+glimpse(meg)
+
+is.na(meg)
+
+sum(is.na(meg))
+
 
